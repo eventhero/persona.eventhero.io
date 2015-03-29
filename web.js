20c877e39193776422f2b7c623e85ef8c7ebb893
Kliment Mamykin <kmamyk@gmail.com>
2015-03-29 10:56:26 -0400

    Updated for express4

--- web.js
+++ web.js
@@ -2,40 +2,44 @@
  * Module dependencies.
  */
 var express = require('express')
-  , passport = require('passport')
-  , site = require('./site')
-  , oauth2 = require('./oauth2')
-  , user = require('./user')
-  , client = require('./client')
-  , util = require('util')
-  
-  
+    , morgan = require('morgan')
+    , bodyParser = require('body-parser')
+    , cookieParser = require('cookie-parser')
+    , session = require('express-session')
+    , errorhandler = require('errorhandler')
+    , passport = require('passport')
+    , site = require('./site')
+    , oauth2 = require('./oauth2')
+    , user = require('./user')
+    , client = require('./client')
+    , util = require('util')
+
 // Express configuration
-  
-var app = express.createServer();
+
+var app = express();
 app.set('view engine', 'ejs');
-app.use(express.logger());
-app.use(express.cookieParser());
-app.use(express.bodyParser());
-app.use(express.session({ secret: 'keyboard cat' }));
+app.use(morgan('combined'));
+app.use(cookieParser('secret')) // TODO: specify options https://www.npmjs.com/package/cookie
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(session({ secret: 'keyboard cat' })) // TODO: specify session cookie options https://github.com/expressjs/session
+
 /*
-app.use(function(req, res, next) {
-  console.log('-- session --');
-  console.dir(req.session);
-  //console.log(util.inspect(req.session, true, 3));
-  console.log('-------------');
-  next()
-});
-*/
+ app.use(function(req, res, next) {
+ console.log('-- session --');
+ console.dir(req.session);
+ //console.log(util.inspect(req.session, true, 3));
+ console.log('-------------');
+ next()
+ });
+ */
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(app.router);
-app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.use(errorhandler());
 
 // Passport configuration
 
 require('./auth');
-
 
 app.get('/', site.index);
 app.get('/login', site.loginForm);
