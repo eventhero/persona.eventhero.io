87cbcf24f9eda5d4b6da4a290c26da99d0786df0
Kliment Mamykin <kmamyk@gmail.com>
2015-03-31 10:42:47 -0400

    Restructured oauth2.js routing

--- routes/oauth2.js
+++ routes/oauth2.js
@@ -152,70 +152,63 @@ server.exchange(oauth2orize.exchange.clientCredentials(function (client, scope, 
   })
 }))
 
-var oauth2 = {}
-// user authorization endpoint
-//
-// `authorization` middleware accepts a `validate` callback which is
-// responsible for validating the client making the authorization request.  In
-// doing so, is recommended that the `redirectURI` be checked against a
-// registered value, although security requirements may vary accross
-// implementations.  Once validated, the `done` callback must be invoked with
-// a `client` instance, as well as the `redirectURI` to which the user will be
-// redirected after an authorization decision is obtained.
-//
-// This middleware simply initializes a new authorization transaction.  It is
-// the application's responsibility to authenticate the user and render a dialog
-// to obtain their approval (displaying details about the client requesting
-// authorization).  We accomplish that here by routing through `ensureLoggedIn()`
-// first, and rendering the `dialog` view.
-
-oauth2.authorization = [
-  login.ensureLoggedIn(),
-  server.authorization(function (clientID, redirectURI, done) {
-    db.clients.findByClientId(clientID, function (err, client) {
-      if (err) { return done(err) }
-      // WARNING: For security purposes, it is highly advisable to check that
-      //          redirectURI provided by the client matches one registered with
-      //          the server.  For simplicity, this example does not.  You have
-      //          been warned.
-      return done(null, client, redirectURI)
-    })
-  }),
-  function (req, res) {
-    res.render('dialog', { transactionID: req.oauth2.transactionID, user: req.user, client: req.oauth2.client })
-  }
-]
-
-// user decision endpoint
-//
-// `decision` middleware processes a user's decision to allow or deny access
-// requested by a client application.  Based on the grant type requested by the
-// client, the above grant middleware configured above will be invoked to send
-// a response.
-
-oauth2.decision = [
-  login.ensureLoggedIn(),
-  server.decision()
-]
-
-// token endpoint
-//
-// `token` middleware handles client requests to exchange authorization grants
-// for access tokens.  Based on the grant type being exchanged, the above
-// exchange middleware will be invoked to handle the request.  Clients must
-// authenticate when making requests to this endpoint.
-
-oauth2.token = [
-  passport.authenticate(['basic', 'oauth2-client-password'], { session: false }),
-  server.token(),
-  server.errorHandler()
-]
-
 module.exports = function () {
   // return express router that can be mounted on a path, e.g. /oauth
   var router = express.Router()
-  router.get('/authorize', oauth2.authorization)
-  router.post('/authorize/decision', oauth2.decision)
-  router.post('/token', oauth2.token)
+
+  // user authorization endpoint
+  //
+  // `authorization` middleware accepts a `validate` callback which is
+  // responsible for validating the client making the authorization request.  In
+  // doing so, is recommended that the `redirectURI` be checked against a
+  // registered value, although security requirements may vary accross
+  // implementations.  Once validated, the `done` callback must be invoked with
+  // a `client` instance, as well as the `redirectURI` to which the user will be
+  // redirected after an authorization decision is obtained.
+  //
+  // This middleware simply initializes a new authorization transaction.  It is
+  // the application's responsibility to authenticate the user and render a dialog
+  // to obtain their approval (displaying details about the client requesting
+  // authorization).  We accomplish that here by routing through `ensureLoggedIn()`
+  // first, and rendering the `dialog` view.
+  router.get('/authorize',
+    login.ensureLoggedIn(),
+    server.authorization(function (clientID, redirectURI, done) {
+      db.clients.findByClientId(clientID, function (err, client) {
+        if (err) { return done(err) }
+        // WARNING: For security purposes, it is highly advisable to check that
+        //          redirectURI provided by the client matches one registered with
+        //          the server.  For simplicity, this example does not.  You have
+        //          been warned.
+        return done(null, client, redirectURI)
+      })
+    }),
+    function (req, res) {
+      res.render('dialog', { transactionID: req.oauth2.transactionID, user: req.user, client: req.oauth2.client })
+    }
+  )
+
+  // user decision endpoint
+  //
+  // `decision` middleware processes a user's decision to allow or deny access
+  // requested by a client application.  Based on the grant type requested by the
+  // client, the above grant middleware configured above will be invoked to send
+  // a response.
+  router.post('/authorize/decision',
+    login.ensureLoggedIn(),
+    server.decision()
+  )
+
+  // token endpoint
+  //
+  // `token` middleware handles client requests to exchange authorization grants
+  // for access tokens.  Based on the grant type being exchanged, the above
+  // exchange middleware will be invoked to handle the request.  Clients must
+  // authenticate when making requests to this endpoint.
+  router.post('/token',
+    passport.authenticate(['basic', 'oauth2-client-password'], { session: false }),
+    server.token(),
+    server.errorHandler()
+  )
   return router
 }
